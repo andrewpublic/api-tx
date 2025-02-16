e8cb8beb94ab8627dbd62329b8c408123c0243f9
andrewpublic <andrew.cho1992@gmail.com>
2025-02-16 19:43:21 +1100

    TDD - refactor Hello function

--- backend/handler.go
+++ backend/handler.go
@@ -2,8 +2,16 @@ package main
 
 import "fmt"
 
-func Hello() string {
-    return "Hello, world"
+func Hello(name ...string) (string, error) {
+    if len(name) > 1 {
+        return "", fmt.Errorf("too many arguments in variadic function")
+    }
+
+    if len(name) == 0 {
+        return "Hello, world!", nil
+    }
+
+    return fmt.Sprintf("Hello, %s!", name[0]), nil
 }
 
 func main() {
--- backend/handler_test.go
+++ backend/handler_test.go
@@ -3,10 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-    got := Hello()
-    want := "Hello, world!"
+    t.Run("SubTest 1: Detailed hello world", func(t *testing.T) {
+        got, _ := Hello("Andrew")
+        want := "Hello, Andrew!"
 
-    if got != want {
-        t.Errorf("got %q want %q", got, want)
-    }
+        if got != want {
+            t.Errorf("got %q want %q", got, want)
+        }
+    })
+    t.Run("SubTest2: Default hello world", func(t *testing.T) {
+        got, _ := Hello()
+        want := "Hello, world!"
+
+        if got != want {
+            t.Errorf("got %q want %q", got, want)
+        }
+    })
 }
