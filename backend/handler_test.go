d906dcd26dd05ed1bda3dd304f1efcdcc5c51748
andrewpublic <andrew.cho1992@gmail.com>
2025-02-16 19:51:36 +1100

    Quii.gitbook - TDD - add comments

--- backend/handler_test.go
+++ backend/handler_test.go
@@ -6,17 +6,19 @@ func TestHello(t *testing.T) {
     t.Run("SubTest 1: Detailed hello world", func(t *testing.T) {
         got, _ := Hello("Andrew")
         want := "Hello, Andrew!"
-
-        if got != want {
-            t.Errorf("got %q want %q", got, want)
-        }
+        assertCorrectMessage(t, got, want)
     })
     t.Run("SubTest2: Default hello world", func(t *testing.T) {
         got, _ := Hello()
         want := "Hello, world!"
-
-        if got != want {
-            t.Errorf("got %q want %q", got, want)
-        }
+        assertCorrectMessage(t, got, want)
     })
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+    t.Helper() // tells test runner to report line number from where this
+               // assert helper function is called
+    if got != want {
+        t.Errorf("got %q want %q", got, want)
+    }
 }
