becafb5bbc34081fb44d33113ecc521bfff396d2
andrewpublic <andrew.cho1992@gmail.com>
2025-02-19 23:41:03 +1100

    Add s3 bucket policy to allow cloudfront

--- infra/frontend/s3.tf
+++ infra/frontend/s3.tf
@@ -15,3 +15,27 @@ resource "aws_s3_object" "frontend" {
   etag   = filemd5("${local.frontend_static_path}/${each.value}")
 }
 
+resource "aws_s3_bucket_policy" "frontend_cloudfront" {
+  bucket = aws_s3_bucket.frontend.id
+  policy = jsonencode({
+    "Version" : "2008-10-17",
+    "Id" : "PolicyForCloudFrontPrivateContent",
+    "Statement" : [
+      {
+        "Sid" : "AllowCloudFrontServicePrincipal",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "cloudfront.amazonaws.com"
+        },
+        "Action" : "s3:GetObject",
+        "Resource" : "${aws_s3_bucket.frontend.arn}/*",
+        "Condition" : {
+          "StringEquals" : {
+            "AWS:SourceArn" : aws_cloudfront_distribution.frontend.arn
+          }
+        }
+      }
+    ]
+  })
+}
+
