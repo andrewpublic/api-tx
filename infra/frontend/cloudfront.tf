4acf7cbbf95d76d36929a23268d4b067496f5b9d
andrewpublic <andrew.cho1992@gmail.com>
2025-02-20 00:07:37 +1100

    fixup! Add custom response headers cloudfront

--- infra/frontend/cloudfront.tf
+++ infra/frontend/cloudfront.tf
@@ -52,8 +52,9 @@ resource "aws_cloudfront_distribution" "frontend" {
 
   ordered_cache_behavior {
     path_pattern = "*.html"
-    #     allowed_methods  = ["GET", "HEAD"]
-    #    cached_methods   = ["GET", "HEAD"]
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    viewer_protocol_policy = "redirect-to-https"
     target_origin_id = local.s3_origin_id
 
     response_headers_policy_id = aws_cloudfront_response_headers_policy.frontend_html.id
