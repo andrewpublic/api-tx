5ba769a24fa1d30f9e7cca59dcd164efa84ceee7
andrewpublic <andrew.cho1992@gmail.com>
2025-02-19 23:46:43 +1100

    Enable S3 static web hosting

--- infra/frontend/s3.tf
+++ infra/frontend/s3.tf
@@ -39,3 +39,23 @@ resource "aws_s3_bucket_policy" "frontend_cloudfront" {
   })
 }
 
+resource "aws_s3_bucket_website_configuration" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+  routing_rule {
+    condition {
+      key_prefix_equals = "docs/"
+    }
+    redirect {
+      replace_key_prefix_with = "documents/"
+    }
+  }
+}
