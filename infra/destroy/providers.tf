1e22959288eef732bcd0283862e602c4685e3aa1
andrewpublic <andrew.cho1992@gmail.com>
2025-02-23 12:33:52 +1100

    Destroy

---
+++ infra/destroy/providers.tf
@@ -0,0 +1,22 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+
+  default_tags {
+    tags = {
+      CreatedBy = "Terraform"
+    }
+  }
+}
+
